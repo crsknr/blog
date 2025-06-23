f29421d445ae7742360e7ae4b1197dc97dab5c3c
crsknr <christoph.kiener@live.de>
2025-06-23 21:31:57 +0200

    fix v18

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ import remarkFootnotes from 'remark-footnotes';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/blog/",
+  basePath: "/blog",
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   output: 'export',
   images: {
